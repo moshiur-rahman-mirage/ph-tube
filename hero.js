d74de85c1e285e715b28698bd6c672cb2415a815
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-09-02 00:52:56 +0600

    wanted to do few cleanup. but turs out i am have more to learn about dry method.

--- hero.js
+++ hero.js
@@ -1,53 +1,51 @@
-
-const loadCategory= async()=>{
-    const res=await fetch('https://openapi.programming-hero.com/api/videos/categories');
+const loadCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
     const data = await res.json();
-    const categories=data.data;
-   
+    const categories = data.data;
+
     displayCategory(categories);
 }
 
-const displayCategory=(categories)=>{
-    const categoryContainer=document.getElementById('cat-container');
-    categoryContainer.innerHTML='';
+const displayCategory = (categories) => {
+    const categoryContainer = document.getElementById('cat-container');
+    categoryContainer.innerHTML = '';
     categories.forEach(category => {
-        const categoryButton=document.createElement('div');
-       categoryButton.classList=`bg-[rgba(37, 37, 37, 0.2)] text-gray-900`;
-       categoryButton.innerHTML=`
+        const categoryButton = document.createElement('div');
+        categoryButton.classList = `bg-[rgba(37, 37, 37, 0.2)] text-gray-900`;
+        categoryButton.innerHTML = `
         <button class="btn" onclick="filterCard(${category.category_id})">${category.category}</button>
        `;
-       categoryContainer.appendChild(categoryButton);
-     
+        categoryContainer.appendChild(categoryButton);
+
     });
 }
 
 
 // Load card
 
-const loadCard=async(id=1001,isSorted)=>{
-    const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
-    const data= await res.json();
-    const cards=data.data;
-    const loadedcard=document.getElementById('loaded-id');
-    loadedcard.innerHTML=id
-    displayCard(cards,isSorted);
-    
+const loadCard = async (id = 1001, isSorted) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await res.json();
+    const cards = data.data;
+    const loadedcard = document.getElementById('loaded-id');
+    loadedcard.innerHTML = id
+    displayCard(cards, isSorted);
+
 }
 
 
-const displayCard=(cards,isSorted)=>{
-    const cardContainer=document.getElementById('card-container');
-    cardContainer.innerHTML=``;
+const displayCard = (cards, isSorted) => {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = ``;
 
-    if (isSorted){
-        console.log('hh')
+    if (isSorted) {
         cards.sort((a, b) => {
             let fa = a.others.views,
                 fb = b.others.views;
-               
-                let aa=parseFloat(fa.substring(-1,fa.length-1)),
-                    bb=parseFloat(fb.substring(-1,fb.length-1));
-                    console.log(aa,bb)
+
+            let aa = parseFloat(fa.substring(-1, fa.length - 1)),
+                bb = parseFloat(fb.substring(-1, fb.length - 1));
+            console.log(aa, bb)
             if (aa < bb) {
                 return 1;
             }
@@ -58,9 +56,9 @@ const displayCard=(cards,isSorted)=>{
         });
     }
 
-    if (cards.length<1){
-        const notfound=document.createElement('div');
-        notfound.innerHTML=`
+    if (cards.length < 1) {
+        const notfound = document.createElement('div');
+        notfound.innerHTML = `
         <div class='text-center flex items-center flex-col'>
             <img src="./icon/icon.png"/>
             <h1 class="text-5xl  font-extrabold mt-5">
@@ -69,20 +67,20 @@ const displayCard=(cards,isSorted)=>{
             </div>
         `
         cardContainer.classList.remove('md:grid', 'lg:grid', 'lg:grid-cols-3', 'xl:grid-cols-4', 'md:grid-cols-2');
-        cardContainer.appendChild(notfound);  
+        cardContainer.appendChild(notfound);
 
-    }else{
-    cards.forEach(card=>{
-        // console.log(card)
-        // card.sort()
-        const second=card.others?.posted_date
-        const xtime=time(second);
-        const isVerified=card.authors[0].verified;
-        const cardBody=document.createElement('div');
-        cardBody.innerHTML=`
+    } else {
+        cards.forEach(card => {
+            // console.log(card)
+            // card.sort()
+            const second = card.others?.posted_date
+            const xtime = time(second);
+            const isVerified = card.authors[0].verified;
+            const cardBody = document.createElement('div');
+            cardBody.innerHTML = `
         <div class="card w-72 h-80 bg-base-100 shadow-xl">
         <figure><img src="${card.thumbnail}" alt="" class="h-52 rounded-b-md" /></figure>
-        <div id="${'time'+card.authors[0].profile_name}" class="text-white w-32 mt-[-35px] ml-[150px] text-right bg-black rounded-md hidden">${xtime}</div>
+        <div id="${'time' + card.authors[0].profile_name}" class="text-white w-32 mt-[-35px] ml-[150px] text-right bg-black rounded-md hidden">${xtime}</div>
         <div class="card-body flex flex-row lg:w-80 md:w-64">
             
         <div class="">
@@ -106,97 +104,57 @@ const displayCard=(cards,isSorted)=>{
             </div>
 
         `;
-        cardContainer.classList.add('md:grid', 'lg:grid', 'lg:grid-cols-3', 'xl:grid-cols-4', 'md:grid-cols-2');
-         cardContainer.appendChild(cardBody);  
-         
-         if (isVerified){
-           const tick=document.getElementById(card.authors[0].profile_name);
-            tick.classList.remove('hidden');
-         }
+            cardContainer.classList.add('md:grid', 'lg:grid', 'lg:grid-cols-3', 'xl:grid-cols-4', 'md:grid-cols-2');
+            cardContainer.appendChild(cardBody);
 
-         if (second>0){
-            // console.log('here')
-            const timedata=document.getElementById(`${'time'+card.authors[0].profile_name}`)
-            timedata.classList.remove('hidden')
-         }
+            if (isVerified) {
+                const tick = document.getElementById(card.authors[0].profile_name);
+                tick.classList.remove('hidden');
+            }
 
-        
-    })
+            if (second > 0) {
+                // console.log('here')
+                const timedata = document.getElementById(`${'time' + card.authors[0].profile_name}`)
+                timedata.classList.remove('hidden')
+            }
+
+
+        })
+    }
+
 }
 
-}
 
 
-
-const time=(second)=>{
-    const xsec=second%60;
+const time = (second) => {
+    const xsec = second % 60;
     // console.log(second);
-     const totalmin=(second-xsec)/60;
-     const min=totalmin%60;
-     const totalhour=(totalmin-min)/60;
-     const hour=totalhour%24;
-     const day=(totalhour-hour)/24;
-     const newday=day%365;
-     const year=(day-newday)/365;
-     if (totalhour<24){
-     return `${totalhour} hrs ${min} min ago`;
-     }else if(day<365){
+    const totalmin = (second - xsec) / 60;
+    const min = totalmin % 60;
+    const totalhour = (totalmin - min) / 60;
+    const hour = totalhour % 24;
+    const day = (totalhour - hour) / 24;
+    const newday = day % 365;
+    const year = (day - newday) / 365;
+    if (totalhour < 24) {
+        return `${totalhour} hrs ${min} min ago`;
+    } else if (day < 365) {
         return `${day}days ${hour} hrs ${min} min ago`;
-     }else{
+    } else {
 
-            return `${year} years ${newday}days ${hour} hrs ${min} min ago`;
-     }
+        return `${year} years ${newday}days ${hour} hrs ${min} min ago`;
+    }
 }
 
-const filterCard=(id)=>{
-   loadCard(id);
-  
+const filterCard = (id) => {
+    loadCard(id);
+
 }
 
-const sortedByView= async()=>{
-    const currentlyLoaded=document.getElementById('loaded-id').innerHTML;
-    loadCard(currentlyLoaded,true);
-    // const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${currentlyLoaded}`);
-    // const data= await res.json();
-    // const cards=data.data;
-    // const cardContainer=document.getElementById('card-container');
-    // cardContainer.innerHTML=``;
-    // cards.sort((a, b) => {
-    //     let fa = a.others.views,
-    //         fb = b.others.views;
-           
-    //         let aa=parseFloat(fa.substring(-1,fa.length-1)),
-    //             bb=parseFloat(fb.substring(-1,fb.length-1));
-    //             console.log(aa,bb)
-    //     if (aa < bb) {
-    //         return -1;
-    //     }
-    //     if (aa > bb) {
-    //         return 1;
-    //     }
-    //     return 0;
-    // });
+const sortedByView = async () => {
+    const currentlyLoaded = document.getElementById('loaded-id').innerHTML;
+    loadCard(currentlyLoaded, true);
 
-
-
-   
-    // cards.forEach(card=>{
-    //     viewc=card.others.views;
-    //     viewc=viewc.substring(-1,viewc.length-1);
-    //     const sortedObject = Object.entries(card.others.views).sort((x, y) => x[1] - y[1]);
-    //     console.log(sortedObject);
-    //     // newarray.push(viewc);
-    // })
-    // newarray.sort(function(a, b){return b - a});
-    
-    // newarray.forEach(item=>{
-        
-    // })
-
-    
-    // console.log(sortedObject);
-
-   
 }
 
 loadCategory();
